7fb34e5ce487849e73a8f403eb07661a849e0050
ulmauull <rizkimaul31@gmail.com>
2022-12-27 14:55:35 +0700

    main

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#0b6623",
+        dark: "#475569",
+        secondary: "#006a4e",
+      },
+      screen: {
+        "2xl": "1320px",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
